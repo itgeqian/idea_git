2a6aa6babcbcef5b5c48a009b0999be8e51e1f50
itgeqian <2933909350@qq.com>
2024-05-22 18:25:08 +0800

    I'm Geqian

--- src/main/java/geqian/Hello.java
+++ src/main/java/geqian/Hello.java
@@ -3,5 +3,6 @@ package geqian;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("I'm Geqian.");
     }
 }
