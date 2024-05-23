1de92a04992b1cb03bff31d0c0b34f302bf295e7
itgeqian <2933909350@qq.com>
2024-05-23 19:45:46 +0800

    完成模块1的开发

--- src/main/java/geqian/Hello.java
+++ src/main/java/geqian/Hello.java
@@ -4,5 +4,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("I'm Geqian.");
+        System.out.println("完成模块1的开发");
     }
 }
